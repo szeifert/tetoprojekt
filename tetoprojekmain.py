185c7e807b91c07b5dd0d27e1bbbf6790297704a
Szeifert Nóra <szeifert.nora@gmail.com>
2020-10-14 23:06:20 +0200

    program

--- tetoprojekmain.py
+++ tetoprojekmain.py
@@ -1,10 +1,11 @@
+import math
+
 print("(Bartha-Nyiri Adrienn, Subicz Adrian, Szeifert Nora, Szikszai Akos)\n")
 
 a = 9.6  #tető a oldal hossza
 b = 3.2   #tető b oldal hossza
 c = 23    #tető magassága
 
-import math
 
 d = math.sqrt( (a/2)**2 + b**2 )  #a tető d oldalának hossza
 
@@ -16,4 +17,6 @@ cs = 0.25 * 0.12 #cserép területe
 
 print('Egy darab cserép területe:',cs,'\n')
 
-print(math.ceil( (et * 1.1) / cs) , "db cserép kell a tető befedéséhez +10%-kal")
+darab = math.ceil( (et * 1.1) / cs)
+
+print(darab, "db cserép kell a tető befedéséhez +10%-kal")
